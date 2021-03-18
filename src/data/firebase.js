0e806127b3e159cec5cf4a7d851df05fa6bc1c45
Diphany <diphany.chea@gmail.com>
2021-03-17 23:26:09 -0400

    code final

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,7 +4,12 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyCrGJyIA7H47_TjmKGeYFkEbAzNpd6Z-7c",
+  authDomain: "ex3-produits-b5a0e.firebaseapp.com",
+  projectId: "ex3-produits-b5a0e",
+  storageBucket: "ex3-produits-b5a0e.appspot.com",
+  messagingSenderId: "988717438369",
+  appId: "1:988717438369:web:2018faa21571fd55f3e7e0"
 };
 
 // Initialiser Firebase
@@ -16,3 +21,4 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+export default bd;
